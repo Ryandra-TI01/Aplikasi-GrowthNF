d0f22b2a47c06d5a88c0268fa64accd0ce641a34
Ryandra-TI01 <ryandra.athaya1705@gmail.com>
2025-05-04 15:01:02 +0700

    initialization

---
+++ database/migrations/2025_05_04_062314_create_mentoring_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mentoring_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mentor_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('mentee_id')->constrained('users')->onDelete('cascade');
+            $table->enum('status', ['pending', 'accepted', 'rejected', 'done'])->default('pending');
+            $table->dateTime('schedule_time');
+            $table->string('platform');
+            $table->text('notes')->nullable();
+            $table->string('proof_photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mentoring_requests');
+    }
+};
